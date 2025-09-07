95aca7a4d86f99dc297c3be60a02d9c51a9ce15d
Nelson Lai <me@nelsonlai.dev>
2025-09-08 01:27:03 +0800

    refactor: update domain

--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -68,7 +68,7 @@ export const GET = (req: Request) => {
           </div>
           <div tw='mt-20 text-5xl font-semibold'>{title}</div>
         </div>
-        <div tw='flex justify-end text-3xl font-bold'>one-blog.nelsonlai.me</div>
+        <div tw='flex justify-end text-3xl font-bold'>one-blog.nelsonlai.dev</div>
       </div>
     ),
     {
